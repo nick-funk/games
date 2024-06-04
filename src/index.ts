11e85017f4e8c616816d5a86f02ea51b8bcf690f
nick-funk <code@nickfunk.com>
2024-06-03 18:23:27 -0600

    use javascript to autoplay video previews

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+const playVideos = () => {
+  const videoElements = document.getElementsByClassName("video");
+  for (const el of videoElements) {
+    const video = el as HTMLVideoElement;
+    if (!video) {
+      continue;
+    }
+
+    video.play();
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("test");
+  setTimeout(playVideos, 150);
 });
